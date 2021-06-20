ac5d643710e946041d681214c2d9308ab8cd42ec
EiZarChiKyaw9 <eizarchikyaw8594@gmail.com>
2021-06-20 14:33:35 +0800

    initial

---
+++ todo_list/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from .forms import Listform
+from .models import list
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def home(request):
+    if request.method == 'POST':
+        form = Listform(request.POST or None)
+        if form.is_valid():
+            form.save()
+
+        all_items = list.objects.all
+        messages.success(request, ('Item has been added to the list!'))
+        return render(request, 'home.html', {'all_items': all_items})
+    else:
+        all_items = list.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
+
+
+def delete(request, list_id):
+    item = list.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted!'))
+    return redirect('home')
+
+
+def cross_off(request, list_id):
+    item = list.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    all_items = list.objects.all
+    return redirect('home')
+
+def uncross(request, list_id):
+    item = list.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    all_items = list.objects.all
+    return redirect('home')
+
+def edit(request, list_id ):
+
+    if request.method == 'POST':
+        item = list.objects.get(pk=list_id)
+
+
+        form = Listform(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item has been updated!'))
+            return redirect('home')
+
+    else:
+        item = list.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
+
+def logon(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('successful login!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Wrong username and password!'))
+            return redirect('logon')
+    else:
+        return render(request, 'authenticate/User_Logon.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out!')
+    return redirect('logon')
